e8ed823a81912682950d636b1ea949a323a0600a
foujitailai <foujitailaimail@163.com>
2018-07-08 00:59:46 +0800

    做个记录

---
+++ src/extension.ts
@@ -0,0 +1,46 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+
+function composeTimeStamp(date: Date): string {
+    return date.toLocaleString();
+}
+
+function replaceEditorSelection(text: string) {
+    const editor = vscode.window.activeTextEditor;
+    if (editor === undefined) { return; }
+    const selections = editor.selections;
+
+    editor.edit((editBuilder) => {
+        selections.forEach((selection) => {
+            editBuilder.replace(selection, '');
+            editBuilder.insert(selection.active, text);
+        });
+    });
+}
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "inserttime" is now active!');
+
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = [
+        vscode.commands.registerCommand('extension.sayHello', () => {
+            let timeStamp: string = composeTimeStamp(new Date());
+            replaceEditorSelection(timeStamp);
+    })];
+
+    context.subscriptions.push(...disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
